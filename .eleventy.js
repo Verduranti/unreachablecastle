6041183b0ec528d5751814ba8e8c64db1cc5d84a
verduranti <angelofchaos@gmail.com>
2021-05-16 23:55:54 -0700

    No really, I want my custom 404

--- .eleventy.js
+++ .eleventy.js
@@ -57,7 +57,7 @@ module.exports = function(config) {
       callbacks: {
         ready: function(err, bs) {
           bs.addMiddleware("*", (req, res) => {
-            const content_404 = fs.readFileSync('public/404/index.html');
+            const content_404 = fs.readFileSync('public/404.html');
             // Add 404 http status code in request header.
             res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
             // Provides the 404 content without redirect.
---
+++ src/src.11tydata.js
@@ -0,0 +1,3 @@
+module.exports = {
+  permalink: "{{page.filePathStem | replace('/src/', '/')}}.html"
+};
